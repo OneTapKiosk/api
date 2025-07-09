56e0f807c01d2af56f46c7723784a82ebf605ea1
liverforpresent <dailynsteady@gmail.com>
2025-07-09 12:30:37 +0900

    feat: Cart Api 생성

---
+++ src/main/kotlin/com/liveforpresent/cookiosk/api/cart/command/presentation/CartCommandController.kt
@@ -0,0 +1,42 @@
+package com.liveforpresent.cookiosk.api.cart.command.presentation
+
+import com.liveforpresent.cookiosk.api.cart.command.domain.entity.CartItem
+import com.liveforpresent.cookiosk.shared.core.presentation.BaseApiResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/cart")
+class CartCommandController {
+    // Return type: cartId
+    @PostMapping
+    fun createCart(): ResponseEntity<BaseApiResponse<Unit>> {
+        return ResponseEntity(HttpStatus.CREATED)
+    }
+
+    @PostMapping("/{cartId}/items")
+    fun addItem(@PathVariable cartId: String, @RequestBody addItemReqDto: Any): ResponseEntity<BaseApiResponse<Unit>> {
+        return ResponseEntity(HttpStatus.CREATED)
+    }
+
+    @PatchMapping("/{cartId}/items/{itemId}")
+    fun updateItemQuantity(
+        @PathVariable cartId: String,
+        @PathVariable itemId: String,
+        @RequestBody updateItemQuantityReqDto: Any
+    ): ResponseEntity<BaseApiResponse<Unit>> {
+        return ResponseEntity(HttpStatus.OK)
+    }
+
+    @DeleteMapping("/{cartId}/items/{itemId}")
+    fun deleteItem(@PathVariable cartId: String, @PathVariable itemId: String): ResponseEntity<BaseApiResponse<Unit>> {
+        return ResponseEntity(HttpStatus.NO_CONTENT)
+    }
+}
