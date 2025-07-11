0a8356f1ea4d695f4679818493c94cbcd448f01c
liverforpresent <dailynsteady@gmail.com>
2025-07-11 22:18:01 +0900

    feat: test 환경 구축

---
+++ src/test/kotlin/com/liveforpresent/cookiosk/api/cart/command/infrastructure/CartCommandRepositoryImplTest.kt
@@ -0,0 +1,9 @@
+package com.liveforpresent.cookiosk.api.cart.command.infrastructure
+
+import com.liveforpresent.cookiosk.api.cart.command.domain.CartCommandRepository
+
+class CartCommandRepositoryImplTest(
+    private val cartCommandRepository: CartCommandRepository
+) {
+
+}
