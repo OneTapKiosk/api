983ea413b88ed4facedb9574453579489b6fa14a
liverforpresent <dailynsteady@gmail.com>
2025-07-18 16:42:40 +0900

    feat: product 목록 조건 검색 api 구현

--- src/main/kotlin/com/liveforpresent/cookiosk/api/product/query/presentation/ProductQueryController.kt
+++ src/main/kotlin/com/liveforpresent/cookiosk/api/product/query/presentation/ProductQueryController.kt
@@ -3,9 +3,11 @@ package com.liveforpresent.cookiosk.api.product.query.presentation
 import com.liveforpresent.cookiosk.api.product.query.application.handler.GetProductByBarcodeHandler
 import com.liveforpresent.cookiosk.api.product.query.application.handler.GetProductByIdHandler
 import com.liveforpresent.cookiosk.api.product.query.application.handler.GetProductByNameHandler
+import com.liveforpresent.cookiosk.api.product.query.application.handler.GetProductListByCriteriaHandler
 import com.liveforpresent.cookiosk.api.product.query.application.query.GetProductByBarcodeQuery
 import com.liveforpresent.cookiosk.api.product.query.application.query.GetProductByIdQuery
 import com.liveforpresent.cookiosk.api.product.query.application.query.GetProductByNameQuery
+import com.liveforpresent.cookiosk.api.product.query.application.query.GetProductListByCriteriaQuery
 import com.liveforpresent.cookiosk.api.product.query.domain.ProductModel
 import com.liveforpresent.cookiosk.shared.core.presentation.BaseApiResponse
 import org.springframework.http.HttpStatus
@@ -19,20 +21,30 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/product")
 class ProductQueryController(
+    private val getProductListByCriteriaHandler: GetProductListByCriteriaHandler,
     private val getProductByIdHandler: GetProductByIdHandler,
     private val getProductByNameHandler: GetProductByNameHandler,
     private val getProductByBarcodeHandler: GetProductByBarcodeHandler
 ) {
     @GetMapping
-    fun getProductList(
-        @RequestParam(required = false) category: String?,
+    fun getProductListByCriteria(
+        @RequestParam(required = false) categoryId: Long?,
         @RequestParam(required = false) name: String?,
         @RequestParam(required = false) minPrice: Int?,
         @RequestParam(required = false) maxPrice: Int?,
         @RequestParam(required = false) sortBy: String?,
         @RequestParam(required = false) inStock: Boolean?
-    ): ResponseEntity<BaseApiResponse<Unit>> {
-        return ResponseEntity(HttpStatus.OK)
+    ): ResponseEntity<BaseApiResponse<List<ProductModel>>> {
+        val query = GetProductListByCriteriaQuery(name, minPrice, maxPrice, categoryId, sortBy)
+        val result = getProductListByCriteriaHandler.execute(query)
+
+        val response = BaseApiResponse<List<ProductModel>>(
+            success = true,
+            message = "상품 목록 조회 성공",
+            data = result
+        )
+
+        return ResponseEntity.status(HttpStatus.OK).body(response)
     }
 
     @GetMapping("/{productId}")
