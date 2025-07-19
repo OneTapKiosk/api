0d463eac799bd999a9364b241e59d09c9c5e16e7
liverforpresent <dailynsteady@gmail.com>
2025-07-19 20:53:25 +0900

    feat: kiosk id로 kiosk 조회 api 구현

---
+++ src/main/kotlin/com/liveforpresent/cookiosk/api/kiosk/query/application/query/GetKioskByIdQuery.kt
@@ -0,0 +1,5 @@
+package com.liveforpresent.cookiosk.api.kiosk.query.application.query
+
+data class GetKioskByIdQuery(
+    val kioskId: Long
+)
--- src/main/kotlin/com/liveforpresent/cookiosk/api/kiosk/query/presentation/KioskQueryController.kt
+++ src/main/kotlin/com/liveforpresent/cookiosk/api/kiosk/query/presentation/KioskQueryController.kt
@@ -1,5 +1,8 @@
 package com.liveforpresent.cookiosk.api.kiosk.query.presentation
 
+import com.liveforpresent.cookiosk.api.kiosk.query.application.handler.GetKioskByIdHandler
+import com.liveforpresent.cookiosk.api.kiosk.query.application.query.GetKioskByIdQuery
+import com.liveforpresent.cookiosk.api.kiosk.query.domain.KioskModel
 import com.liveforpresent.cookiosk.shared.core.presentation.BaseApiResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -10,14 +13,25 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/kiosk")
-class KioskQueryController {
+class KioskQueryController(
+    private val getKioskByIdHandler: GetKioskByIdHandler
+) {
     @GetMapping
     fun getKioskList(): ResponseEntity<BaseApiResponse<Unit>> {
         return ResponseEntity(HttpStatus.OK)
     }
 
     @GetMapping("{kioskId}")
-    fun getKioskDetail(@PathVariable("kioskId") id: String): ResponseEntity<BaseApiResponse<Unit>> {
-        return ResponseEntity(HttpStatus.OK)
+    fun getKioskDetail(@PathVariable("kioskId") id: Long): ResponseEntity<BaseApiResponse<KioskModel>> {
+        val query = GetKioskByIdQuery(id)
+        val result = getKioskByIdHandler.execute(query)
+
+        val response = BaseApiResponse<KioskModel>(
+            success = true,
+            message = "키오스크 정보 조회 성공",
+            data = result
+        )
+
+        return ResponseEntity.status(HttpStatus.OK).body(response)
     }
 }
