715099744f9e5dfbac66be64db6ba5857c5e5a5e
liverforpresent <dailynsteady@gmail.com>
2025-07-25 16:55:14 +0900

    feat: 장바구니 내 상품 삭제/수량 변경 api 구현

--- src/main/kotlin/com/liveforpresent/cookiosk/api/cart/command/application/handler/RemoveItemHandler.kt
+++ src/main/kotlin/com/liveforpresent/cookiosk/api/cart/command/application/handler/RemoveItemHandler.kt
@@ -1,7 +1,7 @@
 package com.liveforpresent.cookiosk.api.cart.command.application.handler
 
 import com.liveforpresent.cookiosk.api.cart.command.application.command.RemoveCartItemCommand
-import com.liveforpresent.cookiosk.api.cart.command.domain.CartCommandRepository
+import com.liveforpresent.cookiosk.api.cart.command.domain.CartCommandRedisRepository
 import com.liveforpresent.cookiosk.api.cart.command.domain.vo.CartItemId
 import com.liveforpresent.cookiosk.shared.core.domain.DomainEventPublisher
 import org.springframework.stereotype.Service
@@ -9,16 +9,17 @@ import org.springframework.transaction.annotation.Transactional
 
 @Service
 class RemoveItemHandler(
-    val cartCommandRepository: CartCommandRepository,
+    //val cartCommandRepository: CartCommandRepository,
+    val cartCommandRedisRepository: CartCommandRedisRepository,
     val eventPublisher: DomainEventPublisher
 ) {
     @Transactional
     fun execute(command: RemoveCartItemCommand) {
-        val cart = cartCommandRepository.findById(command.cartId)
+        val cart = cartCommandRedisRepository.findById(command.cartId)
 
         val updatedCart = cart.removeItem(CartItemId(command.cartItemId))
 
-        cartCommandRepository.save(updatedCart)
+        cartCommandRedisRepository.save(updatedCart)
 
         eventPublisher.publish(updatedCart)
     }
