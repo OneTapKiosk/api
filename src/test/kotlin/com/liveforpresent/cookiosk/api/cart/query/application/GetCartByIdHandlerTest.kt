95c1a61130a279162718a0bba8038b01a97386cb
liverforpresent <dailynsteady@gmail.com>
2025-08-25 01:05:44 +0900

    feat: GetCartByIdHandler 테스트 코드 작성

---
+++ src/test/kotlin/com/liveforpresent/cookiosk/api/cart/query/application/GetCartByIdHandlerTest.kt
@@ -0,0 +1,60 @@
+package com.liveforpresent.cookiosk.api.cart.query.application
+
+import com.liveforpresent.cookiosk.api.cart.command.domain.vo.CartId
+import com.liveforpresent.cookiosk.api.cart.query.application.handler.GetCartByIdHandler
+import com.liveforpresent.cookiosk.api.cart.query.application.query.GetCartByIdQuery
+import com.liveforpresent.cookiosk.api.cart.query.domain.CartItemModel
+import com.liveforpresent.cookiosk.api.cart.query.domain.CartModel
+import com.liveforpresent.cookiosk.api.cart.query.domain.CartQueryRedisRepository
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import java.time.Instant
+
+class GetCartByIdHandlerTest : DescribeSpec({
+    val cartQueryRedisRepository = mockk<CartQueryRedisRepository>()
+    val getCartByIdHandler = GetCartByIdHandler(cartQueryRedisRepository)
+
+    val cartId = CartId(1L)
+    val now = Instant.now()
+
+    describe("getCartByIdHandler execute") {
+        context("장바구니가 존재하는 경우") {
+            val cartModel = CartModel(
+                id = "1L",
+                cartItems = setOf<CartItemModel>(),
+                totalPrice = 10,
+                createdAt = now,
+                updatedAt = now,
+                kioskId = 1L
+            )
+            val query = GetCartByIdQuery(cartId.value)
+
+            every { cartQueryRedisRepository.findById(any()) } returns cartModel
+
+            it("CartModel을 반환해야 한다.") {
+                val result = getCartByIdHandler.execute(query)
+
+                verify(exactly = 1) { cartQueryRedisRepository.findById(query.cartId) }
+                result shouldBe cartModel
+            }
+        }
+
+        context("장바구니가 존재하지 않는 경우") {
+            val invalidCartId = CartId(999L)
+            val query = GetCartByIdQuery(invalidCartId.value)
+
+            every { cartQueryRedisRepository.findById(any()) } throws IllegalStateException()
+
+            it("IllegalArgumentException을 던진다") {
+                shouldThrow<IllegalStateException> {
+                    getCartByIdHandler.execute(query)
+                }
+                verify(exactly = 1) { cartQueryRedisRepository.findById(query.cartId) }
+            }
+        }
+    }
+})
