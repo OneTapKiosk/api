9ed6914d8f82bb392f1edddc458da2f650acc055
liverforpresent <dailynsteady@gmail.com>
2025-08-26 20:21:30 +0900

    feat: OrderView에 order item mapping을 위해 OrderItemListConverter 구현

---
+++ src/main/kotlin/com/liveforpresent/cookiosk/api/order/query/infrastructure/converter/OrderItemListConverter.kt
@@ -0,0 +1,22 @@
+package com.liveforpresent.cookiosk.api.order.query.infrastructure.converter
+
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.liveforpresent.cookiosk.api.order.query.infrastructure.OrderItemView
+import jakarta.persistence.AttributeConverter
+import jakarta.persistence.Converter
+
+@Converter
+class OrderItemListConverter : AttributeConverter<List<OrderItemView>, String> {
+    private val objectMapper = jacksonObjectMapper()
+
+    override fun convertToDatabaseColumn(attribute: List<OrderItemView>?): String {
+        return objectMapper.writeValueAsString(attribute)
+    }
+
+    override fun convertToEntityAttribute(dbData: String?): List<OrderItemView> {
+        return dbData?.let {
+            objectMapper.readValue(it, object : TypeReference<List<OrderItemView>>() {})
+        } ?: emptyList()
+    }
+}
