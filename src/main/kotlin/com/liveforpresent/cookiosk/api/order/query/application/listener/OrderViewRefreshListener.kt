5d57a8f6b133bb16dbaf6918088de08ef7520775
liverforpresent <dailynsteady@gmail.com>
2025-08-26 21:30:09 +0900

    feat: order view 갱신 listener 구현

---
+++ src/main/kotlin/com/liveforpresent/cookiosk/api/order/query/application/listener/OrderViewRefreshListener.kt
@@ -0,0 +1,16 @@
+package com.liveforpresent.cookiosk.api.order.query.application.listener
+
+import com.liveforpresent.cookiosk.api.order.command.domain.event.OrderCreatedEvent
+import com.liveforpresent.cookiosk.api.order.query.application.handler.RefreshOrderViewHandler
+import org.springframework.stereotype.Component
+import org.springframework.transaction.event.TransactionalEventListener
+
+@Component
+class OrderViewRefreshListener(
+    private val refreshOrderViewHandler: RefreshOrderViewHandler,
+) {
+    @TransactionalEventListener
+    fun handleCreate(event: OrderCreatedEvent) {
+        refreshOrderViewHandler.execute()
+    }
+}
