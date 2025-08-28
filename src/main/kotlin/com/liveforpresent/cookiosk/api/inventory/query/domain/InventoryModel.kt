389de6968bf576c1fefa186f9ed370b896ed4f2f
liverforpresent <dailynsteady@gmail.com>
2025-08-28 15:51:55 +0900

    chore: inventory model의 id계열 return type을 string으로 변경

--- src/main/kotlin/com/liveforpresent/cookiosk/api/inventory/query/domain/InventoryModel.kt
+++ src/main/kotlin/com/liveforpresent/cookiosk/api/inventory/query/domain/InventoryModel.kt
@@ -3,10 +3,10 @@ package com.liveforpresent.cookiosk.api.inventory.query.domain
 import java.time.Instant
 
 data class InventoryModel(
-    val id: Long,
+    val id: String,
     val isAvailable: Boolean,
     val quantity: Int,
-    val productId: Long,
+    val productId: String,
     val createdAt: Instant,
     val updatedAt: Instant,
 )
