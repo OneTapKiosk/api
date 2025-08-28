f079616dd982ad2af83effade6f2816eb7460f82
liverforpresent <dailynsteady@gmail.com>
2025-08-29 00:44:03 +0900

    feat: kiosk view 구현 및 repository 수정

--- src/main/kotlin/com/liveforpresent/cookiosk/api/kiosk/query/domain/KioskQueryRepository.kt
+++ src/main/kotlin/com/liveforpresent/cookiosk/api/kiosk/query/domain/KioskQueryRepository.kt
@@ -3,4 +3,5 @@ package com.liveforpresent.cookiosk.api.kiosk.query.domain
 interface KioskQueryRepository {
     fun findById(id: Long): KioskModel
     fun findByCompanyId(companyId: Long): List<KioskModel>
+    fun refreshView()
 }
--- src/main/kotlin/com/liveforpresent/cookiosk/api/kiosk/query/infrastructure/KioskQueryRepositoryImpl.kt
+++ src/main/kotlin/com/liveforpresent/cookiosk/api/kiosk/query/infrastructure/KioskQueryRepositoryImpl.kt
@@ -2,14 +2,16 @@ package com.liveforpresent.cookiosk.api.kiosk.query.infrastructure
 
 import com.liveforpresent.cookiosk.api.kiosk.query.domain.KioskModel
 import com.liveforpresent.cookiosk.api.kiosk.query.domain.KioskQueryRepository
+import jakarta.persistence.EntityManager
 import org.springframework.stereotype.Repository
 
 @Repository
 class KioskQueryRepositoryImpl(
-    private val kioskQueryJpaRepository: KioskQueryJpaRepository
+    private val kioskQueryJpaRepository: KioskQueryJpaRepository,
+    private val em: EntityManager
 ): KioskQueryRepository {
     override fun findById(id: Long): KioskModel {
-        val kioskEntity = kioskQueryJpaRepository.findByIdAndIsDeletedFalse(id)
+        val kioskEntity = kioskQueryJpaRepository.findById(id)
             .orElseThrow { IllegalArgumentException("해당 키오스크가 존재 하지 않습니다.") }
 
         return KioskModel(
@@ -23,7 +25,7 @@ class KioskQueryRepositoryImpl(
     }
 
     override fun findByCompanyId(companyId: Long): List<KioskModel> {
-        val kioskEntities = kioskQueryJpaRepository.findByCompanyIdAndIsDeletedFalse(companyId)
+        val kioskEntities = kioskQueryJpaRepository.findByCompanyId(companyId)
 
         return kioskEntities.map { KioskModel(
             name = it.name,
@@ -33,5 +35,10 @@ class KioskQueryRepositoryImpl(
             devices = it.devices,
             companyId = it.companyId.toString()
         ) }
+    }
+
+    override fun refreshView() {
+        em.createNativeQuery("REFRESH MATERIALIZED VIEW CONCURRENTLY vw_kiosk")
+            .executeUpdate()
     }
 }
