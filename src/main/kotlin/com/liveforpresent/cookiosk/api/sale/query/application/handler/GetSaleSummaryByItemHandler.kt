4b186310b7b3ab16cb302ed9c7561159bc12cb17
liverforpresent <dailynsteady@gmail.com>
2025-08-30 00:17:25 +0900

    fix: SaleByItemModel 수정

--- src/main/kotlin/com/liveforpresent/cookiosk/api/sale/query/application/handler/GetSaleSummaryByItemHandler.kt
+++ src/main/kotlin/com/liveforpresent/cookiosk/api/sale/query/application/handler/GetSaleSummaryByItemHandler.kt
@@ -2,15 +2,15 @@ package com.liveforpresent.cookiosk.api.sale.query.application.handler
 
 import com.liveforpresent.cookiosk.api.sale.query.application.query.GetSaleSummaryByItemQuery
 import com.liveforpresent.cookiosk.api.sale.query.domain.SaleByItemModel
-import com.liveforpresent.cookiosk.api.sale.query.domain.SaleQueryRepository
+import com.liveforpresent.cookiosk.api.sale.query.domain.SaleByItemQueryRepository
 import org.springframework.stereotype.Service
 
 @Service
 class GetSaleSummaryByItemHandler(
-    private val saleQueryRepository: SaleQueryRepository
+    private val saleQueryByItemRepository: SaleByItemQueryRepository
 ) {
     fun execute(query: GetSaleSummaryByItemQuery): List<SaleByItemModel> {
-        return saleQueryRepository.findSummaryByItem(
+        return saleQueryByItemRepository.findByItem(
             query.startAt,
             query.endAt,
             query.kioskId
--- src/main/kotlin/com/liveforpresent/cookiosk/api/sale/query/domain/SaleByItemModel.kt
+++ src/main/kotlin/com/liveforpresent/cookiosk/api/sale/query/domain/SaleByItemModel.kt
@@ -1,7 +1,7 @@
 package com.liveforpresent.cookiosk.api.sale.query.domain
 
-interface SaleByItemModel {
-    fun getName(): String
-    fun getTotalPrice(): Long
-    fun getTotalQuantity(): Long
-}
+class SaleByItemModel(
+    val name: String,
+    val totalQuantity: Int,
+    val totalPrice: Int
+)
