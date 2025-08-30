1e4eff9ed4a0c108cb4dd0e7ce37ce0a038380b3
liverforpresent <dailynsteady@gmail.com>
2025-08-30 21:24:35 +0900

    feat: inventory exception 정의 및 적용

--- src/main/kotlin/com/liveforpresent/cookiosk/shared/exception/CustomExceptionCode.kt
+++ src/main/kotlin/com/liveforpresent/cookiosk/shared/exception/CustomExceptionCode.kt
@@ -23,4 +23,11 @@ enum class CustomExceptionCode(
     ORDER_ITEM_PRICE_NEGATIVE(HttpStatus.BAD_REQUEST, "[OrderItem] 상품 가격은 음수일 수 없습니다."),
 
     ORDER_STATUS_INVALID_STATE(HttpStatus.BAD_REQUEST, "[OrderStatus] 주문 상태가 유효하지 않습니다."),
+
+    // Inventory
+    INVENTORY_QUANTITY_NEGATIVE(HttpStatus.BAD_REQUEST, "[Inventory] 재고 수량은 음수일 수 없습니다."),
+    INVENTORY_DECREASE_AMOUNT_NON_POSITIVE(HttpStatus.BAD_REQUEST, "[Inventory] 감소 수량은 0보다 커야 합니다."),
+    INVENTORY_INSUFFICIENT_STOCK(HttpStatus.BAD_REQUEST, "[Inventory] 재고가 부족합니다."),
+
+    INVENTORY_NOT_FOUND(HttpStatus.NOT_FOUND, "[InventoryRepository] 해당 재고가 존재하지 않습니다."),
 }
