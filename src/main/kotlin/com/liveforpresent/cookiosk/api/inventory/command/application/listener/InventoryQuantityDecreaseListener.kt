330cc3cb8b6d53c5ff447478aa1be01f0aff0c00
liverforpresent <dailynsteady@gmail.com>
2025-09-04 00:16:33 +0900

    fix: InventoryQuantityDecreaseListener가 주문 성공 이벤트를 listen하도록 변경

--- src/main/kotlin/com/liveforpresent/cookiosk/api/inventory/command/application/listener/InventoryQuantityDecreaseListener.kt
+++ src/main/kotlin/com/liveforpresent/cookiosk/api/inventory/command/application/listener/InventoryQuantityDecreaseListener.kt
@@ -1,8 +1,8 @@
 package com.liveforpresent.cookiosk.api.inventory.command.application.listener
 
-import com.liveforpresent.cookiosk.api.cart.command.domain.event.CartItemAddedEvent
 import com.liveforpresent.cookiosk.api.inventory.command.application.command.DecreaseInventoryQuantityCommand
 import com.liveforpresent.cookiosk.api.inventory.command.application.handler.DecreaseInventoryQuantityHandler
+import com.liveforpresent.cookiosk.api.order.command.domain.event.OrderFinishedAsSuccessEvent
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionalEventListener
 
@@ -11,8 +11,13 @@ class InventoryQuantityDecreaseListener(
     private val decreaseInventoryQuantityHandler: DecreaseInventoryQuantityHandler
 ) {
     @TransactionalEventListener
-    fun handle(event: CartItemAddedEvent) {
-        val command = DecreaseInventoryQuantityCommand(event.productId)
-        decreaseInventoryQuantityHandler.execute(command)
+    fun handle(event: OrderFinishedAsSuccessEvent) {
+        event.saleItems.map {
+            val command = DecreaseInventoryQuantityCommand(
+                productId = it.productId.value,
+                amount = it.quantity
+            )
+            decreaseInventoryQuantityHandler.execute(command)
+        }
     }
 }
