c6484a0e00d96d6f3fe9c5f62d5bd4e46f051a78
liverforpresent <dailynsteady@gmail.com>
2025-09-15 00:17:15 +0900

    feat: Company 도메인 테스트 구현

---
+++ src/test/kotlin/com/liveforpresent/cookiosk/api/company/command/domain/CompanyTest.kt
@@ -0,0 +1,35 @@
+package com.liveforpresent.cookiosk.api.company.command.domain
+
+import com.liveforpresent.cookiosk.api.company.command.domain.vo.CompanyId
+import com.liveforpresent.cookiosk.api.company.command.domain.vo.RegistrationNumber
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import java.time.Instant
+
+class CompanyTest: DescribeSpec({
+    describe("Company") {
+        val companyId = CompanyId(1L)
+        val registrationNumber = RegistrationNumber.create("1234567890")
+
+        val companyProps = CompanyProps(
+            registrationNumber = registrationNumber,
+            phone = "1234567890",
+            email = "aaaa@aaaaa.com",
+            createdAt = Instant.now(),
+            isDeleted = false,
+            deletedAt = null
+        )
+
+        describe("create") {
+            val company = Company.create(companyId, companyProps)
+
+            company.id shouldBe companyId
+            company.registrationNumber shouldBe registrationNumber
+            company.email shouldBe companyProps.email
+            company.phone shouldBe companyProps.phone
+            company.createdAt shouldBe Instant.now()
+            company.isDeleted shouldBe false
+            company.deletedAt shouldBe null
+        }
+    }
+})
