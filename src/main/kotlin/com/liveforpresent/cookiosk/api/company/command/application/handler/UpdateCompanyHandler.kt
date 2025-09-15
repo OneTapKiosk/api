5b7e3f54f8727c9eb225f141645198c1f5a825e9
liverforpresent <dailynsteady@gmail.com>
2025-09-16 00:50:53 +0900

    feat: Company 수정 로직 구현

---
+++ src/main/kotlin/com/liveforpresent/cookiosk/api/company/command/application/handler/UpdateCompanyHandler.kt
@@ -0,0 +1,25 @@
+package com.liveforpresent.cookiosk.api.company.command.application.handler
+
+import com.liveforpresent.cookiosk.api.company.command.application.command.UpdateCompanyCommand
+import com.liveforpresent.cookiosk.api.company.command.domain.CompanyCommandRepository
+import com.liveforpresent.cookiosk.api.company.command.domain.vo.RegistrationNumber
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class UpdateCompanyHandler(
+    private val companyCommandRepository: CompanyCommandRepository
+) {
+    @Transactional
+    fun execute(command: UpdateCompanyCommand) {
+        val company = companyCommandRepository.findOne(command.id)
+
+        val updatedCompany = company.update(
+            newRegistrationNumber = command.newRegistrationNumber?.let{ RegistrationNumber.create(it) } ?: company.registrationNumber,
+            newEmail = command.newEmail ?: company.email,
+            newPhone = command.newPhone ?: company.phone
+        )
+
+        companyCommandRepository.save(updatedCompany)
+    }
+}
