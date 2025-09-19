cb508c7e599f837b4ac0d89373816cbafbcdf1d8
liverforpresent <dailynsteady@gmail.com>
2025-09-19 14:09:09 +0900

    feat: Company Exception 정의

--- src/main/kotlin/com/liveforpresent/cookiosk/shared/exception/CustomExceptionCode.kt
+++ src/main/kotlin/com/liveforpresent/cookiosk/shared/exception/CustomExceptionCode.kt
@@ -65,5 +65,6 @@ enum class CustomExceptionCode(
     COMPANY_INVALID_REGISTRATION_NUMBER(HttpStatus.BAD_REQUEST, "[CompanyRegistrationNumber] 유효하지 않은 사업자등록번호 형식입니다."),
     COMPANY_INVALID_STATUS(HttpStatus.BAD_REQUEST, "[CompanyStatus] 유효하지 않은 사업자 상태 입니다."),
     COMPANY_INVALID_EMAIL(HttpStatus.BAD_REQUEST, "[Company] 유효하지 않은 이메일 입니다."),
-    COMPANY_NOT_FOUND(HttpStatus.NOT_FOUND, "[CompanyRepository] 해당 사업체는 존재하지 않습니다.")
+    COMPANY_NOT_FOUND(HttpStatus.NOT_FOUND, "[CompanyRepository] 해당 사업체는 존재하지 않습니다."),
+    COMPANY_ALREADY_EXISTS(HttpStatus.CONFLICT, "[Company] 해당 사업자 등록번호로 이미 가입된 사업체가 있습니다.")
 }
