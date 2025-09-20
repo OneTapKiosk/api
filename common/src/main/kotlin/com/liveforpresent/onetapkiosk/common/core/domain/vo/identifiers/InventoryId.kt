745b11a5605df2f5be30e36d31a86c0545ca6dc1
liverforpresent <dailynsteady@gmail.com>
2025-09-20 19:53:23 +0900

    chore: id객체들은 common에 포함

---
+++ common/src/main/kotlin/com/liveforpresent/onetapkiosk/common/core/domain/vo/identifiers/InventoryId.kt
@@ -0,0 +1,5 @@
+package com.liveforpresent.onetapkiosk.common.core.domain.vo.identifiers
+
+import com.liveforpresent.onetapkiosk.common.core.domain.vo.Identifier
+
+class InventoryId(value: Long): Identifier<Long>(value)
