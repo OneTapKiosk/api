c3a8223938d6fa615c01c7e37f4d245e7fe6f8f3
liverforpresent <dailynsteady@gmail.com>
2025-09-23 00:39:00 +0900

    feat: ProductModel과 ProductView에 inventory 통합

--- product/src/main/kotlin/com/liveforpresent/onetapkiosk/product/query/infrastructure/ProductView.kt
+++ product/src/main/kotlin/com/liveforpresent/onetapkiosk/product/query/infrastructure/ProductView.kt
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import org.hibernate.annotations.Immutable
+import java.time.Instant
 
 @Entity
 @Immutable
@@ -14,8 +15,12 @@ class ProductView(
     val name: String,
     val price: Int,
     val imageUrl: String,
+    val isAvailable: Boolean,
+    val quantity: Int,
     val displayOrder: Int,
     val barcode: String,
     val description: String?,
-    val categoryId: Long?
+    val categoryId: Long?,
+    val createdAt: Instant,
+    val updatedAt: Instant
 )
