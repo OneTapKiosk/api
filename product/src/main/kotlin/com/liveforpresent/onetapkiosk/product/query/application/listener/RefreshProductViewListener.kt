1bc6e508461052f36a242f5ed97fb44a97e00885
liverforpresent <dailynsteady@gmail.com>
2025-09-25 13:14:33 +0900

    feat: RefreshProductViewListener에 상품 수량 증가/감소 이벤트 추가

--- product/src/main/kotlin/com/liveforpresent/onetapkiosk/product/query/application/listener/RefreshProductViewListener.kt
+++ product/src/main/kotlin/com/liveforpresent/onetapkiosk/product/query/application/listener/RefreshProductViewListener.kt
@@ -1,9 +1,7 @@
 package com.liveforpresent.onetapkiosk.product.query.application.listener
 
 import com.liveforpresent.onetapkiosk.common.core.domain.vo.DomainEvent
-import com.liveforpresent.onetapkiosk.product.command.domain.event.ProductCreatedEvent
-import com.liveforpresent.onetapkiosk.product.command.domain.event.ProductDeletedEvent
-import com.liveforpresent.onetapkiosk.product.command.domain.event.ProductUpdatedEvent
+import com.liveforpresent.onetapkiosk.product.command.domain.event.*
 import com.liveforpresent.onetapkiosk.product.query.application.handler.RefreshProductViewHandler
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionalEventListener
@@ -16,7 +14,9 @@ class RefreshProductViewListener(
         classes = [
             ProductCreatedEvent::class,
             ProductUpdatedEvent::class,
-            ProductDeletedEvent::class
+            ProductDeletedEvent::class,
+            ProductQuantityIncreasedEvent::class,
+            ProductQuantityDecreasedEvent::class
         ]
     )
     fun handle(event: DomainEvent) {
