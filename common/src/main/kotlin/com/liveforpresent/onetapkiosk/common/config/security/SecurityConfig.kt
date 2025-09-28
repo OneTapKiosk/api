28b0493e045111025ba716d5d1ed78d21eb17421
liverforpresent <dailynsteady@gmail.com>
2025-09-29 00:19:18 +0900

    feat: password encoder bean 추가

--- common/src/main/kotlin/com/liveforpresent/onetapkiosk/common/config/security/SecurityConfig.kt
+++ common/src/main/kotlin/com/liveforpresent/onetapkiosk/common/config/security/SecurityConfig.kt
@@ -7,6 +7,8 @@ import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
@@ -35,5 +37,10 @@ class SecurityConfig(
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
 
         return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
     }
 }
