cb96dca3cc0b10d6ae91a64d5b6a686adab50429
liverforpresent <dailynsteady@gmail.com>
2025-09-30 13:10:27 +0900

    feat:JwtAuthenticationFilter에 인증 필요 경로 로직 구현

--- common/src/main/kotlin/com/liveforpresent/onetapkiosk/common/config/security/JwtAuthenticationFilter.kt
+++ common/src/main/kotlin/com/liveforpresent/onetapkiosk/common/config/security/JwtAuthenticationFilter.kt
@@ -1,14 +1,17 @@
 package com.liveforpresent.onetapkiosk.common.config.security
 
-import com.liveforpresent.onetapkiosk.common.config.security.SecurityPathFilter.isPublicPath
+import com.liveforpresent.onetapkiosk.common.config.security.jwt.JwtTokenProvider
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 
 @Component
-class JwtAuthenticationFilter: OncePerRequestFilter() {
+class JwtAuthenticationFilter(
+    private val jwtTokenProvider: JwtTokenProvider
+): OncePerRequestFilter() {
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
@@ -16,9 +19,31 @@ class JwtAuthenticationFilter: OncePerRequestFilter() {
     ) {
         val path = request.requestURI
 
-        if (isPublicPath(path)) {
+        if (SecurityPathFilter.isPublicPath(path)) {
             filterChain.doFilter(request, response)
             return
         }
+
+        val jwt = parseTokenFromHeader(request)
+
+        if (jwt != null) {
+            try {
+                val authentication = jwtTokenProvider.getAuthentication(jwt)
+                SecurityContextHolder.getContext().authentication = authentication
+            } catch (e: Exception) {
+                SecurityContextHolder.clearContext()
+            }
+        }
+
+        filterChain.doFilter(request, response)
+    }
+
+    private fun parseTokenFromHeader(request: HttpServletRequest): String? {
+        val bearerToken = request.getHeader("Authorization")
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7)
+        }
+
+        return null
     }
 }
